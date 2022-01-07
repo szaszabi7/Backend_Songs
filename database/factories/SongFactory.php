0bcbb3d82f0f0539082a96359129e78c72b0621d
Szalai Szabolcs <sz.szalai16@gmail.com>
2022-01-07 14:34:15 +0100

    model, factory, migrate, seeder

---
+++ database/factories/SongFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SongFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->words(3, true),
+            'singer' => $this->faker->name(),
+            'length' => $this->faker->biasedNumberBetween(1,500)
+        ];
+    }
+}
